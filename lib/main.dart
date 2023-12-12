e358ddd52f0d95a613166b3a9385d8ff813a1dbc
adiiwinarnoo <adiwinarno860@gmail.com>
2023-12-12 15:45:21 +0700

    applying alert and finished learn about quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(const MainApp());
@@ -31,20 +36,69 @@ class QuizLer extends StatefulWidget {
 }
 
 class _QuizLerState extends State<QuizLer> {
+  List<Icon> scoreKeeper = [];
+
+  void alertFinishedQuestion() {
+    Alert(
+      context: context,
+      title: 'Finished',
+      desc: 'You\'ve already to finished the question',
+      type: AlertType.info,
+      buttons: [
+        DialogButton(
+          color: Colors.black,
+          child: const Text('CONFIRM',style: TextStyle(color: Colors.white),),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
+      ],
+    ).show();
+  }
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        alertFinishedQuestion();
+        quizBrain.resetNumber();
+        scoreKeeper = [];
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(
+            const Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          scoreKeeper.add(
+            const Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
+        Expanded(
           flex: 6,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
-              child: Text("Adi Winarno is handsome?",
+              child: Text(
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25.0, color: Colors.white),
+                style: const TextStyle(fontSize: 25.0, color: Colors.white),
               ),
             ),
           ),
@@ -58,8 +112,11 @@ class _QuizLerState extends State<QuizLer> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(0),
                   )),
-              onPressed: () {},
-              child: const Text('TRUE',
+              onPressed: () {
+                checkAnswer(true);
+              },
+              child: const Text(
+                'TRUE',
                 style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
             ),
@@ -74,10 +131,22 @@ class _QuizLerState extends State<QuizLer> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(0),
                   )),
-              onPressed: () {},
-              child: const Text('FALSE',
+              onPressed: () {
+                checkAnswer(false);
+              },
+              child: const Text(
+                'FALSE',
                 style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
+            ),
+          ),
+        ),
+        //add for score keeper
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Row(
+              children: scoreKeeper,
             ),
           ),
         ),
